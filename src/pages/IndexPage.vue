8a7cd6d69f3c24e899f929e8c236f9bb16399cea
Braden Wong <bmw02002turbo@gmail.com>
2023-01-18 23:39:11 -0500

    Feat: Working components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,17 +2,21 @@
   <q-page class="row items-center justify-evenly">
     <!-- <MobileTable></MobileTable> -->
     <!-- Await response -->
+    {{ catalog }}
   </q-page>
 </template>
 
-<script lang="ts">
-export default {
-  async preFetch() {
-    const res = await fetch('https://janktable.yaleapps.com/api/catalog');
-    const catalog = await res.json();
-    return { catalog };
-  },
-};
-</script>
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
+async function fetchCatalog() {
+  const res = await fetch('https://janktable.yaleapps.com/api/catalog');
+  const catalog = await res.json();
+  return catalog;
+}
 
-<script setup lang="ts"></script>
+const catalog = ref();
+
+onBeforeMount(async () => {
+  catalog.value = await fetchCatalog();
+});
+</script>
