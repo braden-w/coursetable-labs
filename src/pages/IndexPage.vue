6d03988b1670037335ae72e2cc4d99c7e49d8ab2
Braden Wong <bmw02002turbo@gmail.com>
2023-01-19 10:16:18 -0500

    Feat: Table Has All Columns Available!

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -14,7 +14,7 @@
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query';
 import { Catalog } from 'src/types/catalog';
-import { catalogSchema } from 'src/types/catalogSchema';
+import { catalogKeys } from 'src/types/catalogKeys';
 
 type Column = {
   name: keyof Catalog;
@@ -22,22 +22,22 @@ type Column = {
   field: string;
   sortable: boolean;
 };
-const columns: Column[] = [
-  {
-    name: 'all_course_codes',
-    label: 'all_course_codes',
-    field: 'all_course_codes',
-    sortable: true,
-  },
-];
-// const columns: Column[] = Object.keys(catalogSchema).map(
-//   (key): Column => ({
-//     name: key as keyof Catalog,
-//     label: key,
-//     field: key,
+// const columns: Column[] = [
+//   {
+//     name: 'all_course_codes',
+//     label: 'all_course_codes',
+//     field: 'all_course_codes',
 //     sortable: true,
-//   })
-// );
+//   },
+// ];
+const columns: Column[] = catalogKeys.map(
+  (key): Column => ({
+    name: key as keyof Catalog,
+    label: keyToLabel(key),
+    field: key,
+    sortable: true,
+  })
+);
 const { isLoading, isFetching, isError, data, error } = useQuery({
   queryKey: ['catalog'],
   queryFn: fetchCatalog,
@@ -47,6 +47,14 @@ async function fetchCatalog() {
   const res = await fetch('https://janktable.yaleapps.com/api/catalog');
   const catalog = await res.json();
   return catalog;
+}
+
+// Takes in string like "times_by_day" and returns "Times by Day"
+function keyToLabel(label: string) {
+  return label
+    .split('_')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
 }
 
 // onBeforeMount(async () => {
