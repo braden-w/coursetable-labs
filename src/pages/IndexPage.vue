fe5b2ea4f850a7b94e6a60a2e3ea69d03d4e44fb
Braden Wong <bmw02002turbo@gmail.com>
2023-01-19 09:53:27 -0500

    Feat: Working Mini Table

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,20 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <!-- <MobileTable></MobileTable> -->
-    <q-table :data="data" :columns="columns" row-key="id" />
+    <q-table
+      title="Courses"
+      :rows="data"
+      :columns="columns"
+      row-key="name"
+      :loading="isLoading"
+    />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query';
 import { Catalog } from 'src/types/catalog';
+import { catalogSchema } from 'src/types/catalogSchema';
 
 type Column = {
   name: keyof Catalog;
@@ -15,13 +22,22 @@ type Column = {
   field: string;
   sortable: boolean;
 };
-// For each key in Catalog, create a column
-const columns: Column[] = Object.keys(Catalog).map((key) => ({
-  name: key as keyof Catalog,
-  label: key,
-  field: key,
-  sortable: true,
-}));
+const columns: Column[] = [
+  {
+    name: 'all_course_codes',
+    label: 'all_course_codes',
+    field: 'all_course_codes',
+    sortable: true,
+  },
+];
+// const columns: Column[] = Object.keys(catalogSchema).map(
+//   (key): Column => ({
+//     name: key as keyof Catalog,
+//     label: key,
+//     field: key,
+//     sortable: true,
+//   })
+// );
 const { isLoading, isFetching, isError, data, error } = useQuery({
   queryKey: ['catalog'],
   queryFn: fetchCatalog,
@@ -29,7 +45,7 @@ const { isLoading, isFetching, isError, data, error } = useQuery({
 
 async function fetchCatalog() {
   const res = await fetch('https://janktable.yaleapps.com/api/catalog');
-  const catalog = (await res.json()) as Catalog;
+  const catalog = await res.json();
   return catalog;
 }
 
