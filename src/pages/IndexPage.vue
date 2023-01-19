4484ea1512f72dedbaa92068d473574c06dc4aaa
Braden Wong <bmw02002turbo@gmail.com>
2023-01-19 09:33:39 -0500

    Feat: Vue Query

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,22 +1,39 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <!-- <MobileTable></MobileTable> -->
-    {{ catalog }}
+    <q-table :data="data" :columns="columns" row-key="id" />
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
 import { Catalog } from 'src/types/catalog';
-import { ref, onBeforeMount } from 'vue';
+
+type Column = {
+  name: keyof Catalog;
+  label: string;
+  field: string;
+  sortable: boolean;
+};
+// For each key in Catalog, create a column
+const columns: Column[] = Object.keys(Catalog).map((key) => ({
+  name: key as keyof Catalog,
+  label: key,
+  field: key,
+  sortable: true,
+}));
+const { isLoading, isFetching, isError, data, error } = useQuery({
+  queryKey: ['catalog'],
+  queryFn: fetchCatalog,
+});
+
 async function fetchCatalog() {
   const res = await fetch('https://janktable.yaleapps.com/api/catalog');
   const catalog = (await res.json()) as Catalog;
   return catalog;
 }
 
-const catalog = ref();
-
-onBeforeMount(async () => {
-  catalog.value = await fetchCatalog();
-});
+// onBeforeMount(async () => {
+//   catalog.value = await fetchCatalog();
+// });
 </script>
