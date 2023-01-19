ca2b06fb9a0cc8c4acfcd58833812f41f7bce85b
Braden Wong <bmw02002turbo@gmail.com>
2023-01-18 23:48:26 -0500

    Feat: Catalog

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <!-- <MobileTable></MobileTable> -->
-    <!-- Await response -->
     {{ catalog }}
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { Catalog } from 'src/types/catalog';
 import { ref, onBeforeMount } from 'vue';
 async function fetchCatalog() {
   const res = await fetch('https://janktable.yaleapps.com/api/catalog');
-  const catalog = await res.json();
+  const catalog = (await res.json()) as Catalog;
   return catalog;
 }
 
