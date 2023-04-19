7c2d6382c4a55eb74b3ef6bbe95b99316cbebb8f
Braden Wong <bmw02002turbo@gmail.com>
2023-04-19 16:15:59 -0400

    refactor: pinia encapsulates form data

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -24,10 +24,9 @@
       </q-input>
     </q-card>
     <q-table
-      :rows="coursesStore.courses"
+      :rows="courses"
       :columns="columns"
       row-key="listing_id"
-      :loading="isLoading"
       :visible-columns="visibleColumns"
       virtual-scroll
       v-model:pagination="pagination"
@@ -41,94 +40,23 @@
 <script lang="ts">
 export default {
   async preFetch({ store }) {
-    async function fetchCatalog() {
-      const visibleColumns: CatalogKeys[] = [
-        'course_code',
-        'title',
-        'all_course_codes',
-        'areas',
-        'average_rating',
-        'average_rating_same_professors',
-        'average_workload',
-        'average_workload_same_professors',
-        'average_gut_rating',
-        'average_professor',
-        'subject',
-        'times_by_day',
-      ];
-      const { data, error } = await supabase
-        .from('Courses')
-        .select(visibleColumns.join(','))
-        .eq('season_code', '202303')
-        .limit(100);
-      return data;
-    }
     const coursesStore = useCoursesStore(store);
-    coursesStore.courses = await fetchCatalog();
+    coursesStore.fetchCatalog();
   },
 };
 </script>
 
 <script setup lang="ts">
 import { useCoursesStore } from 'src/stores/courses';
-import { supabase } from 'src/supabase';
-import { CatalogKeys } from 'src/types/catalog';
-import { Course } from 'src/types/course';
 import { ref } from 'vue';
-const coursesStore = useCoursesStore();
+import { storeToRefs } from 'pinia';
 
-type Column = {
-  name: keyof Course;
-  label: string;
-  field: string;
-  sortable: boolean;
-  align: 'left' | 'right' | 'center';
-};
+const coursesStore = useCoursesStore();
+const { courses, columns, visibleColumns, filter, pagination } =
+  storeToRefs(coursesStore);
 
 const showSettings = ref(false);
 function toggleSettings() {
   showSettings.value = !showSettings.value;
 }
-const filter = ref('');
-
-const pagination = ref({
-  rowsPerPage: 0,
-});
-
-const visibleColumns: CatalogKeys[] = [
-  'course_code',
-  'title',
-  'all_course_codes',
-  'areas',
-  'average_rating',
-  'average_rating_same_professors',
-  'average_workload',
-  'average_workload_same_professors',
-  'average_gut_rating',
-  'average_professor',
-  'subject',
-  'times_by_day',
-];
-
-const columns: Column[] = visibleColumns.map(
-  (key): Column => ({
-    name: key,
-    label: keyToLabel(key),
-    field: key,
-    sortable: true,
-    align: 'left',
-  })
-);
-
-// Takes in string like "times_by_day" and returns "Times by Day"
-function keyToLabel(label: string) {
-  return label
-    .split('_')
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(' ');
-}
-
-// onBeforeMount(async () => {
-//   catalog.value = await fetchCatalog();
-// });
 </script>
--- src/stores/courses.ts
+++ src/stores/courses.ts
@@ -1,9 +1,64 @@
 import { defineStore } from 'pinia';
 import { supabase } from 'src/supabase';
+import { CatalogKeys } from 'src/types/catalog';
+import { Course } from 'src/types/course';
+
+type Column = {
+  name: keyof Course;
+  label: string;
+  field: string;
+  sortable: boolean;
+  align: 'left' | 'right' | 'center';
+};
 
 export const useCoursesStore = defineStore('courses', {
   state: () => ({
     courses: [],
+    visibleColumns: [
+      'course_code',
+      'title',
+      'all_course_codes',
+      'areas',
+      'average_rating',
+      'average_rating_same_professors',
+      'average_workload',
+      'average_workload_same_professors',
+      'average_gut_rating',
+      'average_professor',
+      'subject',
+      'times_by_day',
+    ] as CatalogKeys[],
+    filter: '',
+    pagination: { rowsPerPage: 0 },
   }),
-  actions: {},
+  getters: {
+    columns: (state) =>
+      state.visibleColumns.map(
+        (key): Column => ({
+          name: key,
+          label: keyToLabel(key),
+          field: key,
+          sortable: true,
+          align: 'left',
+        })
+      ),
+  },
+  actions: {
+    async fetchCatalog() {
+      const { data, error } = await supabase
+        .from('Courses')
+        .select(this.visibleColumns.join(','))
+        .eq('season_code', '202303')
+        .limit(100);
+      this.courses = data;
+    },
+  },
 });
+
+// Takes in string like "times_by_day" and returns "Times by Day"
+function keyToLabel(label: string) {
+  return label
+    .split('_')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
