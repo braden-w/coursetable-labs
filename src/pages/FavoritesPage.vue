7e1d2f640c7d552b50893f6634071699cce30aac
Braden Wong <bmw02002turbo@gmail.com>
2023-04-20 12:28:47 -0400

    feat: working filtering of course options

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -2,36 +2,58 @@
   <q-select
     v-model="selectedCourses"
     label="Select courses"
-    :options="courseOptions"
+    :options="displayedCourseOptions"
     option-value="course_id"
     option-label="displayText"
     multiple
     clearable
+    use-input
+    @filter="filterFn"
   />
 </template>
 
+<script lang="ts">
+export default {
+  async preFetch({ store }) {
+    const favoritesStore = useFavoritesStore(store);
+    favoritesStore.fetchAbbreviatedCatalog();
+  },
+};
+</script>
 <script setup lang="ts">
+import { useFavoritesStore } from 'src/stores/favorites';
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
 
-const selectedCourses = ref([]);
-const courses = [
-  {
-    course_id: 1,
-    all_course_codes: ['COMP101', 'INFO101'],
-    title: 'Introduction to Computer Science',
-  },
-  {
-    course_id: 2,
-    all_course_codes: ['MATH101', 'STAT101'],
-    title: 'Introduction to Mathematics',
-  },
-  // Add more courses here
-];
+const favoritesStore = useFavoritesStore();
+const { selectedCourses, courseOptions } = storeToRefs(favoritesStore);
 
-const courseOptions = courses.map((course) => {
-  return {
-    course_id: course.course_id,
-    displayText: `${course.all_course_codes.join('/')} - ${course.title}`,
-  };
-});
+const displayedCourseOptions = ref(courseOptions.value);
+
+// const filterFn = (val: string, update: (options: any[]) => void) => {
+//   update(
+//     courseOptions.value.filter((option) => {
+//       return option.displayText.toLowerCase().includes(val.toLowerCase());
+//     })
+//   );
+// };
+
+function filterFn(val: string, update) {
+  // if (val === '') {
+  //   update(() => {
+  //     options.value = stringOptions
+
+  //     // here you have access to "ref" which
+  //     // is the Vue reference of the QSelect
+  //   })
+  //   return
+  // }
+
+  update(() => {
+    const needle = val.toLowerCase();
+    displayedCourseOptions.value = courseOptions.value.filter(
+      (v) => v.displayText.toLowerCase().indexOf(needle) > -1
+    );
+  });
+}
 </script>
