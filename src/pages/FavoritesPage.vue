deae4f19bdb7ff58937a0c12eb79b7c6bf2f4d89
Braden Wong <bmw02002turbo@gmail.com>
2023-04-21 02:15:32 -0400

    feat: spacing

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -3,21 +3,23 @@
     <div class="column items-center">
       <q-card flat class="my-card q-mb-md">
         <q-card-section>
-          <div class="text-h3">What is your favorite courses at Yale?</div>
+          <div class="text-h3 q-mb-md">
+            What is your favorite courses at Yale?
+          </div>
           <div class="text-subtitle1">Please share your course experiences</div>
         </q-card-section>
       </q-card>
 
       <q-card flat class="my-card q-mb-md">
         <q-card-section>
-          <div class="text-h6">What courses have you taken so far?</div>
+          <div class="text-h6 q-mb-md">What courses have you taken so far?</div>
           <q-input standout v-model="coursesTaken" class="q-mt-sm" />
         </q-card-section>
       </q-card>
 
       <q-card flat class="my-card q-mb-md">
         <q-card-section>
-          <div class="text-h6">
+          <div class="text-h6 q-mb-md">
             What courses have been your favorite courses at Yale?
           </div>
           <q-input standout v-model="favoriteCourses" class="q-mt-sm" />
@@ -26,7 +28,7 @@
 
       <q-card flat class="my-card q-mb-md">
         <q-card-section>
-          <div class="text-h6">Any words to defend your choices?</div>
+          <div class="text-h6 q-mb-md">Any words to defend your choices?</div>
           <q-input standout v-model="defendChoices" class="q-mt-sm" />
         </q-card-section>
       </q-card>
