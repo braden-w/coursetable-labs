87925200c06642ef07f95c7d819c838c83ce59c1
Braden Wong <bmw02002turbo@gmail.com>
2023-04-21 03:23:07 -0400

    feat: filled and update text

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -7,7 +7,9 @@
             What is your favorite courses at Yale?
           </div>
           <div class="text-subtitle1 text-weight-light">
-            Please share your course experiences
+            As we wrap up the school year, let's reflect. Please share your
+            course experiences. I wish I could use a Google Form but there are
+            too much
           </div>
         </q-card-section>
       </q-card>
@@ -15,9 +17,9 @@
       <q-card flat class="my-card q-mb-md">
         <q-card-section>
           <div class="text-h6 text-weight-light q-mb-md">
-            What courses have you taken so far?
+            What is your email address?
           </div>
-          <SelectCourses />
+          <q-input filled label="Email" v-model="email" />
         </q-card-section>
       </q-card>
 
@@ -55,6 +57,9 @@ import SelectCourses from 'src/components/SelectCourses.vue';
 import { useFavoritesStore } from 'src/stores/favorites';
 import { ref } from 'vue';
 
+const favoritesStore = useFavoritesStore();
+
+const email = ref('');
 const defendChoices = ref('');
 </script>
 
