a85bfda5c0c0dd06d9dabf47fdd5b01bcbeaee48
Braden Wong <bmw02002turbo@gmail.com>
2023-04-21 02:03:29 -0400

    fix: margins

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,21 +1,21 @@
 <template>
   <q-page class="flex flex-center">
     <div class="column items-center">
-      <q-card flat class="my-card q-ma-sm">
+      <q-card flat class="my-card q-mb-md">
         <q-card-section>
           <div class="text-h4">Yale Course Survey</div>
           <div class="text-subtitle1">Please share your course experiences</div>
         </q-card-section>
       </q-card>
 
-      <q-card flat class="my-card q-ma-sm">
+      <q-card flat class="my-card q-mb-md">
         <q-card-section>
           <div class="text-h6">What courses have you taken so far?</div>
           <q-input outlined v-model="coursesTaken" class="q-mt-sm" />
         </q-card-section>
       </q-card>
 
-      <q-card flat class="my-card q-ma-sm">
+      <q-card flat class="my-card q-mb-md">
         <q-card-section>
           <div class="text-h6">
             What courses have been your favorite courses at Yale?
@@ -24,7 +24,7 @@
         </q-card-section>
       </q-card>
 
-      <q-card flat class="my-card q-ma-sm">
+      <q-card flat class="my-card q-mb-md">
         <q-card-section>
           <div class="text-h6">Any words to defend your choices?</div>
           <q-input outlined v-model="defendChoices" class="q-mt-sm" />
@@ -49,5 +49,8 @@ export default {
 <style>
 .my-card {
   width: 80rem;
+  border-radius: 8px;
+  box-shadow: 0 1px 2px rgba(60, 64, 67, 0.3),
+    0 1px 3px 1px rgba(60, 64, 67, 0.15);
 }
 </style>
