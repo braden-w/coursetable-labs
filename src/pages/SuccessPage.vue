f9335be7b51f8a105087d43e8fd60700f1b2b45d
Braden Wong <bmw02002turbo@gmail.com>
2023-04-26 11:59:59 -0400

    feat: SuccessPage

---
+++ src/pages/SuccessPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <div class="text-h6">Congratulations!</div>
+    <p>
+      Your responses have been recorded and you will be emailed results on May
+      1.
+    </p>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -15,6 +15,11 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/FavoritesPage.vue') },
     ],
   },
+  {
+    path: '/success',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/SuccessPage.vue') }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
